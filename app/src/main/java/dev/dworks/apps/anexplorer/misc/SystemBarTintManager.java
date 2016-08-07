8dfb71ba268905b46f8029c11c40de29faf5097c
Hari Krishna Dulipudi <heart.break.kid.b4u@gmail.com>
2016-08-07 16:24:52 +0530

    remove extra permissions

--- app/src/main/java/dev/dworks/apps/anexplorer/misc/SystemBarTintManager.java
+++ app/src/main/java/dev/dworks/apps/anexplorer/misc/SystemBarTintManager.java
@@ -99,7 +99,7 @@ public class SystemBarTintManager {
             TypedArray a = activity.obtainStyledAttributes(attrs);
             try {
                 mStatusBarAvailable = a.getBoolean(0, false);
-                mNavBarAvailable = a.getBoolean(1, false);
+                //mNavBarAvailable = a.getBoolean(1, false);
             } finally {
                 a.recycle();
             }
