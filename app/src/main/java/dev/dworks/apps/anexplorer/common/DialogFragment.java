8ecdc47a431c3fd379e17106af178ecda70f0a3a
Hari Krishna Dulipudi <heart.break.kid.b4u@gmail.com>
2018-10-03 01:38:54 +1000

    add dialog

--- app/src/main/java/dev/dworks/apps/anexplorer/common/DialogFragment.java
+++ app/src/main/java/dev/dworks/apps/anexplorer/common/DialogFragment.java
@@ -1,10 +1,13 @@
 package dev.dworks.apps.anexplorer.common;
 
+import android.app.Activity;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatDialogFragment;
+
+import android.text.TextUtils;
 import android.widget.Button;
 
 import dev.dworks.apps.anexplorer.misc.CrashReportingManager;
@@ -45,6 +48,39 @@ public class DialogFragment extends AppCompatDialogFragment {
     }
 
     public static void showThemedDialog(AlertDialog.Builder builder){
+        Dialog dialog = builder.create();
+        dialog.show();
+        tintButtons(dialog);
+    }
+
+    public static void showThemedDialog(Activity activity,
+                                        String titleId, String messageId, String positiveButtonId,
+                                        String negativeButtonId,
+                                        final DialogInterface.OnClickListener positiveClickListener,
+                                        final DialogInterface.OnClickListener negativeClickListener){
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        builder.setMessage(messageId)
+                .setCancelable(false)
+                .setPositiveButton(positiveButtonId, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        if (null != positiveClickListener) {
+                            positiveClickListener.onClick(dialog, which);
+                        }
+                    }
+                }).setNegativeButton(negativeButtonId, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                if (null != negativeClickListener) {
+                    negativeClickListener.onClick(dialog, which);
+                }
+            }
+        });
+        if(TextUtils.isEmpty(titleId)){
+            builder.setTitle(titleId);
+        }
         Dialog dialog = builder.create();
         dialog.show();
         tintButtons(dialog);
