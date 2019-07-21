cbdbebb5950ab747b9c6f88183343662809920a2
Hari Krishna Dulipudi <heart.break.kid.b4u@gmail.com>
2019-07-21 10:56:56 +1000

    fix google drive integration for free build

--- app/src/main/java/dev/dworks/apps/anexplorer/fragment/ConnectionsFragment.java
+++ app/src/main/java/dev/dworks/apps/anexplorer/fragment/ConnectionsFragment.java
@@ -16,7 +16,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.PopupMenu;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.loader.app.LoaderManager;
